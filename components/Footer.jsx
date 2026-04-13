c72fa412352958616b644fbcd3bcfbfd650cc9b6
Arutik228 <arutiktopcik@gmail.com>
2026-04-13 23:28:38 +0000

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,9 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row justify-between items-center gap-4">
           <p className="text-gray-400 text-sm">© {currentYear} Арутюн Маркосян. Все права защищены.</p>
           <div className="flex space-x-4">
-            <a href="https://github.com/arutyun" target="_blank" className="text-gray-400 hover:text-primary"><FaGithub size={20} /></a>
-            <a href="https://t.me/arutyun" target="_blank" className="text-gray-400 hover:text-primary"><FaTelegram size={20} /></a>
-            <a href="https://vk.com/arutyun" target="_blank" className="text-gray-400 hover:text-primary"><FaVk size={20} /></a>
-            <a href="mailto:arutyun@example.com" className="text-gray-400 hover:text-primary"><FaEnvelope size={20} /></a>
+            <a href="https://github.com/Arutik228" target="_blank" className="text-gray-400 hover:text-primary"><FaGithub size={20} /></a>
+            <a href="https://t.me/@Arutik52" target="_blank" className="text-gray-400 hover:text-primary"><FaTelegram size={20} /></a>
+            <a href="mailto:arutiktopcik@gmail.com" className="text-gray-400 hover:text-primary"><FaEnvelope size={20} /></a>
           </div>
         </div>
       </div>
